e7b6da4945d81436243a3188664679c5d9031f85
lubgade <lubgade@yahoo.com>
2018-01-17 11:24:47 -0500

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -16,7 +16,7 @@ app.post('/todos', (req, res) => {
     });
 
     todo.save().then((doc) => {
-        res.send(doc)
+        res.send(doc);
     }, (e) => {
         res.status(400).send(e);
     });
@@ -28,6 +28,21 @@ app.get('/todos', (req, res) => {
     }, (e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+
+        res.send({todo});
+    }).catch((e) => res.status(400).send());
 });
 
 app.listen(3000, () => {
