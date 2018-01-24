f4e719c0d078a6c5708a1613cf60eb659a16bc17
lubgade <lubgade@yahoo.com>
2018-01-24 17:20:10 -0500

    Add POST /users/login route

--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -23,6 +23,17 @@ userRouter.get('/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+userRouter.post('/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    //var user = new User(body);
 
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);            
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
 
 module.exports = {userRouter};
